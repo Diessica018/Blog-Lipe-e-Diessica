3f92a09ff0901c67ae5c3aa72163af077c2901e0
Felipe Nunes <123976777+NotLipe@users.noreply.github.com>
2026-01-29 22:11:58 -0300

    Add Post component for managing blog posts
    Refactor App component to include Post and PostFake components, and manage post data with state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { use, useState, useTransition } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Post(){
+  
+  const [title, settitle] = useState(null)
+  const [category, setcategory] = useState(null)
+  const [content, setcontetn] = useState(null)
 
-  return (
+  const postInitialData = [{
+    title: '',
+    category: '',
+    content: '',
+  },
+  {
+    title: 'Pastel',
+    category: 'comida',
+    content: 'pastel e bom demais',
+  }]
+
+  const [filterPost, setfilterPost] = useState(postInitialData)
+
+  function getData(){
+    const postUser = {
+      title: title,
+      category: category,
+      content: content,
+    }
+  const newPost = [...postInitialData, postUser]
+  setfilterPost(newpost)
+  }
+
+return (
+    <form onSubmit={getData}>
+      {filterPost.map((Blog) => {
+        <>
+          <button type="submit">Criar Post</button>
+          <div id="post">
+            <div id="top">
+              <a id="profile">
+                <img id="pfpicture" src="../link.jpeg" alt="" />
+              </a>
+              <h1 id="user">{user}</h1>
+            </div>
+            <h1 id="title">Vaga de Emprego</h1>
+            <div id="content">
+              <p>
+                Recentemente eu me candidatei para uma vaga de programador
+                Junior em uma empresa chamada Cyber Genios, dizem que é muito
+                disputada, será que eu consigo entrar?
+              </p>
+              <img src="" alt="" />
+            </div>
+            <div id="botton">
+              <p id="date">{date}</p>
+              <button id="like">Like</button>
+            </div>
+          </div>
+        </>;
+      })}
+    </form>
+  );
+}
+
+function PostFake() {
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id='post'>
+        <div id='top'>
+          <a id='profile'>F</a>
+          <h1 id='user'>Felipe Nunes</h1>
+        </div>
+        <div id='content'>
+          <p>Esse texto fala da imagem abaixo, muito legal né? Isso é apenas um teste!</p>
+          <img src="" alt="" />
+        </div>
+        <div id='botton'>
+          <p id='date'>26/01/2026</p>
+          <p id='time'>05:05</p>
+        </div>
+        <div id='action'>
+          <button id='like'>Like</button>
+          <button id='dislike'> Dislike</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
+function App(){
+  return(
+    <>
+      <div className='container'>
+        <Post />
+      </div>
+    </>
+  );
+}
 export default App
